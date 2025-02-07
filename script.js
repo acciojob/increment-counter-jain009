79cc11b69502bf06e0cd28fae6ed564143d07a6c
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-07 17:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  let counterValue = 0;
+  
+  const counterElement = document.getElementById("counter");
+  const incrementButton = document.getElementById("incrementBtn");
+
+  incrementButton.addEventListener("click", () => {
+    alert(`Current value before increment: ${counterValue}`);
+    counterValue++;
+    counterElement.textContent = counterValue;
+  });
+});
